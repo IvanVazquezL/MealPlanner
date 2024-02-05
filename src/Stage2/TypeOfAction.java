8feb61909c2ae60e75a0b9a632af87f5d0bf7a3b
IvanVazquezL <ivanvl_11@hotmail.com>
2024-02-04 21:38:05 -0600

    initial files

---
+++ src/Stage2/TypeOfAction.java
@@ -0,0 +1,30 @@
+package Stage2;
+
+import java.util.ArrayList;
+
+public enum TypeOfAction {
+    ADD("ADD"),
+    SHOW("SHOW"),
+    EXIT("EXIT");
+
+    private final String value;
+
+    TypeOfAction(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static ArrayList<String> getStringValues() {
+        TypeOfAction[] actions = TypeOfAction.values();
+        ArrayList<String> stringValues = new ArrayList<>();
+
+        for (TypeOfAction action : actions) {
+            stringValues.add(action.getValue());
+        }
+
+        return stringValues;
+    }
+}
